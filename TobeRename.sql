ebb0e0be5ad07a17123b816a2e7eb42155e45667
stormbroken <181250184@smail.nju.edu.cn>
2019-12-05 18:29:02 +0800

    修正数据库

--- TobeRename.sql
+++ TobeRename.sql
@@ -1,18 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : local
- Source Server Type    : MySQL
- Source Server Version : 50728
- Source Host           : localhost:3306
- Source Schema         : toberename
-information
- Target Server Type    : MySQL
- Target Server Version : 50728
- File Encoding         : 65001
-
- Date: 2/12/2019 10:27:03
-*/
 USE toberename;# 注意需要先新建scheme
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -21,27 +6,26 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`(
 	`id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(25) NOT NULL,
-    `category` int(11) DEFAULT NULL,
     `password` varchar(25) NOT NULL,
-    `createtime` mediumtext,
-    `content` varchar(50) DEFAULT '他很懒，还没有自我介绍',
-    `friendIDs` mediumtext default null,
-    `ban` mediumtext default null,
+    `createtime` mediumtext not null,
+    `content` mediumtext DEFAULT null,
+    `url` mediumtext default null,
+    #`friendIDs` mediumtext default null,
 	`likes` mediumtext default null,
     `history` mediumtext default null,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `information`;
-CREATE TABLE `information`(
+DROP TABLE IF EXISTS `twitter`;
+CREATE TABLE `twitter`(
 	`id` int(11) not null auto_Increment,
     `uid` int(11) not null,
     `uname` varchar(25) not null,
     `content` mediumtext default null,
-    `url` varchar(100) default null,
-    `createtime` mediumtext,
+    `url` mediumtext default null,
+    `createtime` mediumtext not null,
+    `hit` int(11) default null,
     `comments` mediumtext default null,
-    `hits` mediumtext default null,
     PRIMARY KEY (`id`)
 )ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
@@ -49,34 +33,35 @@ DROP TABLE IF EXISTS `cookbook`;
 CREATE TABLE `cookbook`(
 	`id` int(11) not null auto_increment,
     `uid` int(11) not null,
-    `name` varchar(25) not null,
+    `cname` varchar(25) not null,
     `category` varchar(25) not null,
+    `material` mediumtext default null,
     `calorie` int(11) DEFAULT NULL,
     `salt` int(11) default null,
     `suger` int(11) default null,
     `protein` int(11) default null,
     `fat` int(11) default null,
-    `url` varchar(100) default null,
-    `createtime` mediumtext,
+    `url` mediumtext default null,
+    `createtime` mediumtext not null,
+    `hit` int(11) default null,
     `steps` mediumtext default null,
-    `hit` mediumtext default null,
 	`comments` mediumtext default null,
     primary key (`id`)
 )ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
-DROP TABLE IF EXISTS `community`;
-CREATE TABLE `community`(
-	`id` int(11) not null auto_increment,
-    `uid` int(11) not null,
-    `uname` varchar(25) not null,
-    `name` varchar(25) not null,
-    `createtime` mediumtext,
-    `hot` int(11) default 0,
-    PRIMARY KEY(`id`)
-)ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
+#DROP TABLE IF EXISTS `community`;
+#CREATE TABLE `community`(
+#	`id` int(11) not null auto_increment,
+#    `uid` int(11) not null,
+#    `uname` varchar(25) not null,
+#    `name` varchar(25) not null,
+#    `createtime` mediumtext,
+#    `hot` int(11) default 0,
+#    PRIMARY KEY(`id`)
+#)ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
-drop table if exists `userban`;
-create table `userban`(
+drop table if exists `userTaste`;
+create table `userTaste`(
 	`id` int(11) not null auto_increment,
     `uid` int(11) not null,
     `type` int(11) not null,
@@ -84,34 +69,45 @@ create table `userban`(
     primary key(`id`)
 )ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
-drop table if exists `informationComment`;
-create table `informationComment`(
+drop table if exists `twitterComment`;
+create table `twitterComment`(
 	`id` int(11) not null auto_increment,
     `uid` int(11) not null,
+    `tid` int(11) not null,
     `content` mediumtext default null,
     `url` varchar(25) default null,
     `reply` mediumtext default null,
-    `createtime` mediumtext,
+    `createtime` mediumtext not null,
     primary key(`id`)
 )ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
-drop table if exists `cookbookComment`;
-create table `cookbookComment`(
-	`id` int(11) not null auto_increment,
-    `uid` int(11) not null,
-    `content` mediumtext default null,
-    `url` varchar(25) default null,
-    `reply` mediumtext default null,
-    `createtime` mediumtext,
-    primary key(`id`)
-)ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
+#drop table if exists `cookbookComment`;
+#create table `cookbookComment`(
+#	`id` int(11) not null auto_increment,
+#    `uid` int(11) not null,
+#    `content` mediumtext default null,
+#    `url` varchar(25) default null,
+#    `reply` mediumtext default null,
+#    `createtime` mediumtext,
+#    primary key(`id`)
+#)ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
 drop table if exists `cookbookStep`;
 create table `cookbookStep`(
 	`id` int(11) not null auto_increment,
-    `cookbookID` int(11) not null,
+    `cbid` int(11) not null,
     `number` int(11) not null,
+    `content` mediumtext default null,
     `url` varchar(100) default null,
+    primary key(`id`)
+)ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
+
+drop table if exists `log`;
+create table `log`(
+	`id` int(11) not null auto_increment,
+    `uid` int(11) not null,
+    `request` json default null,
+    `response` json default null,
     primary key(`id`)
 )ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
